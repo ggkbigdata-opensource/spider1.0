549fbcb9fe7fca63ecca303a165d3371d5c7207c
caiyu19880 <caiyu19880@126.com>
2016-06-23 14:55:38 +0800

    删除无用的application.java

--- src/test/java/com/ggk/spider/SpiderApplicationTests.java
+++ src/test/java/com/ggk/spider/SpiderApplicationTests.java
@@ -1,9 +1,11 @@
-package com.ggk;
+package com.ggk.spider;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.ggk.spider.SpiderApplication;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = SpiderApplication.class)
